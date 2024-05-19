10f8213ac6f0b1e6276e8eda15310e2740f61ff9
Bespi1 <sergej.bespalov@stud.uni-heidelberg.de>
2024-05-19 20:39:49 +0200

    worked on ex4_1 a little bit

--- eml06/Ex04/exercise4_1.py
+++ eml06/Ex04/exercise4_1.py
@@ -1,15 +1,17 @@
+from __future__ import print_function
+import argparse
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
 import time
-import csv
-import argparse
+import pandas as pd
 
+# Define the VGG11 model
 class VGG11(nn.Module):
     def __init__(self, dropout_p=0.5):
-        super(VGG11, self).__init__()
+        super().__init__()
         self.features = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=3, padding=1), nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
@@ -30,7 +32,7 @@ class VGG11(nn.Module):
             nn.Linear(512 * 1 * 1, 4096), nn.ReLU(inplace=True),
             nn.Dropout(dropout_p),
             nn.Linear(4096, 4096), nn.ReLU(inplace=True),
-            nn.Linear(4096, 10),
+            nn.Linear(4096, 10),  # 10 classes for CIFAR-10
         )
 
     def forward(self, x):
@@ -39,8 +41,11 @@ class VGG11(nn.Module):
         x = self.classifier(x)
         return F.log_softmax(x, dim=1)
 
+# Training function
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
+    train_losses = []
+    train_accs = []
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -52,67 +57,111 @@ def train(args, model, device, train_loader, optimizer, epoch):
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
                 100. * batch_idx / len(train_loader), loss.item()))
+        train_losses.append(loss.item())
+        pred = output.argmax(dim=1, keepdim=True)
+        correct = pred.eq(target.view_as(pred)).sum().item()
+        accuracy = correct / len(data)
+        train_accs.append(accuracy)
+    return train_losses, train_accs
 
-def test(model, device, test_loader, epoch, results):
+# Testing function
+def test(model, device, test_loader, epoch):
     model.eval()
     test_loss = 0
     correct = 0
+    test_losses = []
+    test_accs = []
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            loss = F.nll_loss(output, target, reduction='sum').item()
+            test_loss += loss
             pred = output.argmax(dim=1, keepdim=True)
             correct += pred.eq(target.view_as(pred)).sum().item()
-
+            test_losses.append(loss)
+            accuracy = correct / len(data)
+            test_accs.append(accuracy)
     test_loss /= len(test_loader.dataset)
     accuracy = 100. * correct / len(test_loader.dataset)
-    print('Test Epoch: {}, Average loss: {:.4f}, Accuracy: {}/{} ({:.4f}%)'.format(
+    print('Test Epoch: {}, Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
         epoch, test_loss, correct, len(test_loader.dataset), accuracy))
-    
-    results.append([epoch, test_loss, accuracy])
+    return test_losses, test_accs
 
+# Main function
 def main():
-    parser = argparse.ArgumentParser(description='PyTorch CIFAR10 VGG11 Example')
-    parser.add_argument('--batch-size', type=int, default=128, metavar='N')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N')
-    parser.add_argument('--epochs', type=int, default=30, metavar='N')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR')
-    parser.add_argument('--dropout_p', type=float, default=0.5, metavar='D')
-    parser.add_argument('--no-cuda', action='store_true', default=False)
-    parser.add_argument('--seed', type=int, default=1, metavar='S')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N')
+    # Training settings
+    parser = argparse.ArgumentParser(description='PyTorch CIFAR-10 VGG11 Example')
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=30, metavar='N',
+                        help='number of epochs to train (default: 30)')
+    parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
+                        help='learning rate (default: 0.0001)')
+    parser.add_argument('--dropout_p', type=float, default=0.5, metavar='P',
+                        help='dropout probability (default: 0.5)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='disables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=200, metavar='N',
+                        help='how many batches to wait before logging training status')
     args = parser.parse_args()
-    
     use_cuda = not args.no_cuda and torch.cuda.is_available()
+
     torch.manual_seed(args.seed)
+
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10('../data', train=True, download=True,
-                         transform=transforms.Compose([
-                             transforms.ToTensor(),
-                         ])),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10('../data', train=False, transform=transforms.Compose([
-                             transforms.ToTensor(),
-                         ])),
-        batch_size=args.test_batch_size, shuffle=False, **kwargs)
+    train_kwargs = {'batch_size': args.batch_size}
+    test_kwargs = {'batch_size': args.test_batch_size}
+    if use_cuda:
+        cuda_kwargs = {'num_workers': 2,
+                       'pin_memory': True,
+                       'shuffle': True}
+        train_kwargs.update(cuda_kwargs)
+        test_kwargs.update(cuda_kwargs)
+
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+    ])
+
+    train_dataset = datasets.CIFAR10('../data', train=True, download=True,
+                                     transform=transform)
+    test_dataset = datasets.CIFAR10('../data', train=False, download=True,
+                                    transform=transform)
+    train_loader = torch.utils.data.DataLoader(train_dataset, **train_kwargs)
+    test_loader = torch.utils.data.DataLoader(test_dataset, **test_kwargs)
 
     model = VGG11(dropout_p=args.dropout_p).to(device)
     optimizer = optim.Adam(model.parameters(), lr=args.lr)
-    
-    results = []
+
+    train_losses = []
+    train_accs = []
+    test_losses = []
+    test_accs = []
+
+    print(f'Starting training at: {time.time():.4f}')
     for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(model, device, test_loader, epoch, results)
-    
-    with open('results.csv', mode='w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(['Epoch', 'Test Loss', 'Test Accuracy'])
-        writer.writerows(results)
+        train_loss, train_acc = train(args, model, device, train_loader, optimizer, epoch)
+        test_loss, test_acc = test(model, device, test_loader, epoch)
+        train_losses.extend(train_loss)
+        train_accs.extend(train_acc)
+        test_losses.extend(test_loss)
+        test_accs.extend(test_acc)
+
+    # Save data for plotting does not work for now!!!!!!!!!!!!
+    results = pd.DataFrame({
+        'train_loss': train_losses,
+        'train_acc': train_accs,
+        'test_loss': test_losses,
+        'test_acc': test_accs,
+    })
+    results.to_csv('training_results.csv', index=False)
+    print('Training results saved to training_results.csv')
 
 if __name__ == '__main__':
     main()
--- eml06/Ex04/plot4_1.py
+++ eml06/Ex04/plot4_1.py
@@ -1,22 +1,40 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('results.csv')
-plt.figure(figsize=(10, 5))
+def plot_results(csv_file):
+    # Load the data from CSV file
+    data = pd.read_csv(csv_file)
 
-plt.subplot(1, 2, 1)
-plt.plot(data['Epoch'], data['Test Loss'], label='Test Loss')
-plt.xlabel('Epoch')
-plt.ylabel('Loss')
-plt.title('Test Loss over Epochs')
-plt.legend()
+    # Extract values
+    train_loss = data['train_loss']
+    train_acc = data['train_acc']
+    test_loss = data['test_loss']
+    test_acc = data['test_acc']
 
-plt.subplot(1, 2, 2)
-plt.plot(data['Epoch'], data['Test Accuracy'], label='Test Accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.title('Test Accuracy over Epochs')
-plt.legend()
+    epochs = range(1, len(train_loss) // len(train_acc) + 1)
 
-plt.tight_layout()
-plt.savefig("Ex4_1.pdf")
+    # Plot training and testing loss
+    plt.figure(figsize=(12, 6))
+
+    plt.subplot(1, 2, 1)
+    plt.plot(train_loss, label='Training Loss')
+    plt.plot([i * len(train_acc) for i in epochs], test_loss[:len(epochs)], label='Testing Loss')
+    plt.xlabel('Batch')
+    plt.ylabel('Loss')
+    plt.title('Training and Testing Loss')
+    plt.legend()
+
+    # Plot training and testing accuracy
+    plt.subplot(1, 2, 2)
+    plt.plot(train_acc, label='Training Accuracy')
+    plt.plot([i * len(train_acc) for i in epochs], test_acc[:len(epochs)], label='Testing Accuracy')
+    plt.xlabel('Batch')
+    plt.ylabel('Accuracy')
+    plt.title('Training and Testing Accuracy')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    plot_results('training_results.csv')
