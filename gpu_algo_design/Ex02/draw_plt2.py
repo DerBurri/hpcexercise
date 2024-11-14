757d4cd22dd811d0bf4a16fd3d88c69d0b2cc0c7
DG <mi312@stud.uni-heidelberg.de>
2024-11-14 09:20:13 +0100

    [FEATURE] Ex2_3 graphing glow up

--- gpu_algo_design/Ex02/draw_plt2.py
+++ gpu_algo_design/Ex02/draw_plt2.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3.11
 
 import matplotlib.pyplot as plt
+plt.rc("ytick.major", size=5, width=1)
+plt.rcParams["text.usetex"] = False
+plt.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["font.serif"] = "Times New Roman"
+plt.rcParams["font.size"] = 15
+plt.rcParams["xtick.labelsize"] = 20
+plt.rcParams["ytick.labelsize"] = 20
+plt.rcParams["axes.titlesize"] = 22
+plt.rcParams["axes.labelsize"] = 22
 
 # Define a function to calculate FLOP/point based on radius
 def flop_per_point(radius):
@@ -23,7 +33,7 @@ for line in lines:
     throughput.append(thrpt)
 
 # Initialize subplots for Throughput and FLOP/s
-fig, axs = plt.subplots(2, 1, figsize=(8, 8), layout="tight")
+fig, axs = plt.subplots(2, 1, figsize=(12, 12), layout="tight")
 
 # Throughput Plot
 axs[0].plot(radii, throughput, marker='o', label='Throughput (MPoints/s)')
@@ -35,12 +45,14 @@ axs[0].grid(True)
 # FLOP/sec calculation
 flops_s = [t * flop_per_point(r) * 1e6 for t, r in zip(throughput, radii)]
 
+tflops_s = [f / 1e12 for f in flops_s]
+
 # FLOP/sec Plot
-axs[1].plot(radii, flops_s, marker='s', color='r', label='FLOPS')
+axs[1].plot(radii, tflops_s, marker='s', color='r', label='FLOPS')
 axs[1].set_title('Floating-point operations throughput')
 axs[1].set_xlabel('Radius')
-axs[1].set_ylabel('FLOP/s')
+axs[1].set_ylabel('TFLOP/s')
 axs[1].grid(True)
 
 # Adjust layout and display the plots
-plt.savefig('thrp_ex3.pdf', format='pdf')
+plt.savefig('thrp_ex3.pdf', format='pdf', bbox_inches='tight')
