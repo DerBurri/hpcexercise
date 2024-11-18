dde8fada5f83504246081f3c6d6936ac4daefa15
DG <mi312@stud.uni-heidelberg.de>
2024-11-18 15:29:02 +0100

    [UPDATE] full sweep for 3.2

--- gpu_algo_design/Ex03/run.sh
+++ gpu_algo_design/Ex03/run.sh
@@ -7,12 +7,5 @@
 ##SBATCH --time=01:00:00
 
 # echo "3.2"
-echo "" > threadFenceReduction_multi.txt
-for b in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 16384 32768 65536 131072 262144 524228 1045876 2097152 4194304 8388608 16777216 33554432 67108864 134217728 268435456; do
-    ./threadFenceReduction --n=1048576 --threads=128 --maxblocks=$b --multipass| grep "Bandwidth:" >> threadFenceReduction_multi.txt
-done
-
-echo "" > threadFenceReduction_single.txt
-for b in 1 2 4 8 16 32 64 128 256 512 1024 2048 4096 16384 32768 65536 131072 262144 524228 1045876 2097152 4194304 8388608 16777216 33554432 67108864 134217728 268435456; do
-    ./threadFenceReduction --n=1048576 --threads=128 --maxblocks=$b | grep "Bandwidth:" >> threadFenceReduction_single.txt
-done
+echo "" > out/threadFenceReduction.csv
+./threadFenceReduction/threadFenceReduction --shmoo --maxblocks=65536>> out/threadFenceReduction.csv
